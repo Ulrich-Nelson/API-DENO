163ec4815aaf132dd0a0ea1ab74edd97daad5fdc
Sebastien-Lechat <seb_le@hotmail.fr>
2021-01-04 17:41:31 +0100

    📦 NEW: init project

---
+++ main.ts
@@ -0,0 +1,16 @@
+import * as expressive from 'https://raw.githubusercontent.com/NMathar/deno-express/master/mod.ts';
+import * as jwt from './helpers/jwt.ts';
+
+const port = 8001;
+const app = new expressive.App();
+
+// route with dynamic parameter
+app.get("/", async(req: expressive.Request, res: expressive.Response) => {
+    
+});
+
+(async() => {
+    const server = await app.listen(port);
+    // deno run server.ts --allow-net --allow-read --unstable --isolatedModules
+    console.log("app listening on port " + server.port);
+})();
