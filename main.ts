9fd1061ef0662d03fdb2f4c0718fbe9678cba9e2
Sebastien-Lechat <seb_le@hotmail.fr>
2021-01-11 12:36:16 +0100

    📦 NEW: index & error page

--- main.ts
+++ main.ts
@@ -1,14 +1,23 @@
-import { opine, json, raw, urlencoded } from "https://deno.land/x/opine@1.0.2/mod.ts";;
-import * as jwt from './helpers/jwt.helpers.ts';
 import { RouteIndex } from "./routes/index.ts";
+import { opine, json, urlencoded } from "https://deno.land/x/opine@1.0.2/mod.ts";
 
 const port: number = 8001;
 const app = opine();
 
+const __dirname = new URL('.', import.meta.url).pathname;
+
 app.use(json());
 app.use(urlencoded());
 
+app.get('/', (req, res)=> {
+    res.sendFile( __dirname.substring(1) + 'public/index.html');
+});
+
 app.use( RouteIndex );
+
+app.get('*', (req, res)=> {
+    res.sendFile(__dirname.substring(1) + 'public/error.html');
+});
 
 app.listen(port);
 // deno run --allow-net --allow-read --unstable main.ts
