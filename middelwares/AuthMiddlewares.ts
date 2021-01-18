b4e4450233a7e710cd362c3ce622d67ac67667bf
Ulrich-Nelson <ulrichnelsontapinfo@yahoo.fr>
2021-01-18 02:26:01 +0100

    create a child user

--- middelwares/AuthMiddlewares.ts
+++ middelwares/AuthMiddlewares.ts
@@ -1,7 +1,7 @@
 import * as expressive from 'https://raw.githubusercontent.com/NMathar/deno-express/master/mod.ts';
 import { Application, NextFunction } from "https://deno.land/x/opine@1.0.2/src/types.ts";
 import { opine } from "https://deno.land/x/opine@1.0.2/src/opine.ts";
-import { sendResponse } from '../helpers/response.helpers.ts';
+import { getJwtPayload } from "../helpers/jwt.helpers.ts";
 
 
 
@@ -9,7 +9,7 @@ import { sendResponse } from '../helpers/response.helpers.ts';
 const middleware: Application = opine();
 
 //récupération du header
-const extractBearerToken = (headerValue: any) => {
+const extractBearerToken = (headerValue: string) => {
     if (typeof headerValue !== 'string') {
         return false
     }
@@ -17,14 +17,15 @@ const extractBearerToken = (headerValue: any) => {
     return matches && matches[2]
 }
 
-
-middleware.use((req: Request) => {
+//Vos droits d'accès ne permettent pas d'accéder à la ressource
+middleware.use((req: Request, _res: Response, next: NextFunction) => {
     try {
-    // Récupération du token
+    //Récupération du token
     const token = req.headers && extractBearerToken(req.headers)
-    // Présence d'un token
-    if (!token) throw new Error('Une ou plusieurs données sont erronées');
-    
+    //Présence d'un token
+    if (!token) throw new Error("Vos droits d'accès ne permettent pas d'accéder à la ressource");
+    getJwtPayload(token)
+    next()
     } catch (err) {
     }
 })
