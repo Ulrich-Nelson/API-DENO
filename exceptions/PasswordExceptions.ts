5456202d7773f78b7d247b19fe7d654e74664a6b
Sebastien-Lechat <52241790+Sebastien-Lechat@users.noreply.github.com>
2021-02-06 14:19:34 +0100

    Update PasswordExceptions.ts

--- exceptions/PasswordExceptions.ts
+++ exceptions/PasswordExceptions.ts
@@ -8,7 +8,7 @@ export default class PasswordException extends Error {
      * @param password 
      */
     static isValidPassword(password: string): boolean {
-        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{7,}$/;
         if (password.length < 7 || password.length > 20) return false;
         else return regex.test(password)
     }
